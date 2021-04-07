9f8bfe5491f04c7003f2cb9f97266bbf008e660e
João Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2021-04-07 20:29:40 -0300

    Initial commit

---
+++ src/main/java/br/com/smartservice/notificator/notificator/NotificatorApplication.java
@@ -0,0 +1,13 @@
+package br.com.smartservice.notificator.notificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/smartservice/notificator/notificator/NotificatorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smartservice.notificator.notificator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
