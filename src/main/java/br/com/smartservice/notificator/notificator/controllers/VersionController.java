df27cdfc754ff75b3907b490fbed2ed75ef73652
Joao Victor Fanchini de Azevedo <joaofanchini@gmail.com>
2021-05-05 21:11:31 -0300

    Returning content type

--- src/main/java/br/com/smartservice/notificator/notificator/controllers/VersionController.java
+++ src/main/java/br/com/smartservice/notificator/notificator/controllers/VersionController.java
@@ -3,13 +3,14 @@ package br.com.smartservice.notificator.notificator.controllers;
 import br.com.smartservice.notificator.notificator.dtos.internal.VersionDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("version")
+@RequestMapping(value = "version",produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class VersionController {
     @GetMapping
